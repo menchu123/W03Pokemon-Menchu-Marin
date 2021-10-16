45740380173256641fc94df50368c780c96098ae
Menchu MV <mcmvillalobos@gmail.com>
2021-10-16 09:22:40 +0200

    Extracted pokemon data from API and assigned it in PokeCard component

--- public/js/Poke-card.js
+++ public/js/Poke-card.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-new */
 import Component from "./Component.js";
+import PokeServices from "./Poke-services.js";
 
 class PokeCard extends Component {
   url;
@@ -11,6 +12,19 @@ class PokeCard extends Component {
   constructor(parentElement, url) {
     super(parentElement, "pokemon-card", "li");
     this.url = url;
+
+    (async () => {
+      const pokeServices = new PokeServices(this.url);
+      const getPokeData = await pokeServices.getPokemons(this.url);
+      this.pokeName = getPokeData.name.toUpperCase();
+      this.pokeNumber = getPokeData.id;
+      this.pokeImg = getPokeData.sprites.other.dream_world.front_default;
+      const pokeTypes = getPokeData.types;
+      pokeTypes.forEach((type) => this.pokeType.push(type.type.name));
+      console.log(this.pokeType);
+
+      this.generateHtml();
+    })();
 
     this.generateHtml();
   }
@@ -26,7 +40,7 @@ class PokeCard extends Component {
               <span class="pokemon-card__name">${this.pokeName}</span>
               <ul class="pokemon-card__types">
               ${
-                this.pokeType.length > 2
+                this.pokeType.length < 2
                   ? `<li class="pokemon-card__type">${this.pokeType[0]}</li>`
                   : `<li class="pokemon-card__type">${this.pokeType[0]}</li>
                 <li class="pokemon-card__type">${this.pokeType[1]}</li>`

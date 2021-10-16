a8603f884ba9889505cbc21a63c3eaffb84839e0
Menchu MV <mcmvillalobos@gmail.com>
2021-10-16 13:07:30 +0200

    Added pagination buttons text and passed

---
+++ public/js/test/Pagination.test.js
@@ -0,0 +1,26 @@
+import PaginationButton from "../Pagination";
+
+describe("Given a Pagination class constructor", () => {
+  describe("When it receives a div container, a 'patata' class and a 'test' text", () => {
+    test("Then it should render a button element 'test' text inside the button", () => {
+      const container = document.createElement("div");
+      const text = "test";
+
+      const button = new PaginationButton(container, "patata", text);
+
+      expect(button.element.textContent).toBe(text);
+    });
+  });
+
+  describe("When it receives a div container, a 'patata' class, a 'test' text and a function", () => {
+    test("Then when the user clicks the button, the function should be invoked", () => {
+      const container = document.createElement("div");
+      const action = jest.fn();
+
+      const button = new PaginationButton(container, "patata", "test", action);
+      button.element.dispatchEvent(new Event("click"));
+
+      expect(action).toHaveBeenCalled();
+    });
+  });
+});
